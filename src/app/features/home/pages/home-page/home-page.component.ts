f10588451f37bac4dc47514de5971ec8b2a234f4
shabareenath <shabareenath9876@gmail.com>
2026-06-16 12:03:07 +0530

    offer section updated

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { HeroBannerComponent } from '../../components/hero-banner/hero-banner.component';
 import { CategoriesSectionComponent } from '../../components/categories-section/categories-section.component';
+import { OffersBannerComponent } from '../../components/offers-banner/offers-banner.component';
+import { FeaturedProductsComponent } from '../../components/featured-products/featured-products.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
+import { TrendingProductsComponent } from '../../components/trending-products/trending-products.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroBannerComponent, CategoriesSectionComponent],
+  imports: [
+    HeroBannerComponent,
+    CategoriesSectionComponent,
+    OffersBannerComponent,
+    FeaturedProductsComponent,
+    TestimonialsComponent,
+    TrendingProductsComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
