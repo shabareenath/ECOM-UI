0b6f7d82a2c08714cc0f5955f8ae9131d96b4397
shabareenath <shabareenath9876@gmail.com>
2026-06-16 20:05:04 +0530

    featured completed

--- src/app/features/home/components/featured-products/featured-products.component.ts
+++ src/app/features/home/components/featured-products/featured-products.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './featured-products.component.html',
   styleUrls: ['./featured-products.component.scss'],
 })
-export class FeaturedProductsComponent {}
+export class FeaturedProductsComponent {
+  products = [
+    {
+      id: 1,
+      name: 'Oversized Black Tee',
+      price: 999,
+      image: 'assets/featured1.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Graphic Street Tee',
+      price: 1199,
+      image: 'assets/featured2.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Essential White Tee',
+      price: 899,
+      image: 'assets/featured3.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Premium Oversized Tee',
+      price: 1299,
+      image: 'assets/featured4.jpeg',
+    },
+  ];
+}
--- src/app/features/home/components/trending-products/trending-products.component.ts
+++ src/app/features/home/components/trending-products/trending-products.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './trending-products.component.html',
   styleUrls: ['./trending-products.component.scss'],
 })
-export class TrendingProductsComponent {}
+export class TrendingProductsComponent {
+  products = [
+    {
+      id: 1,
+      name: 'Oversized Black Tee',
+      price: 999,
+      image: 'assets/featured1.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Graphic Street Tee',
+      price: 1199,
+      image: 'assets/featured2.jpeg',
+    },
+    {
+      id: 3,
+      name: 'Essential White Tee',
+      price: 899,
+      image: 'assets/featured3.jpeg',
+    },
+    {
+      id: 4,
+      name: 'Premium Oversized Tee',
+      price: 1299,
+      image: 'assets/featured4.jpeg',
+    },
+  ];
+}
