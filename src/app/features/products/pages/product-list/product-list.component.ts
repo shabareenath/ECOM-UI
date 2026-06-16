ee85170142e3e7cda33e5c6340556ba09be6c792
shabareenath <shabareenath9876@gmail.com>
2026-06-16 21:04:02 +0530

    search updated

--- src/app/features/products/pages/product-list/product-list.component.ts
+++ src/app/features/products/pages/product-list/product-list.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductSearchComponent } from '../../components/product-search/product-search.component';
+import { ProductGalleryComponent } from '../../components/product-gallery/product-gallery.component';
+import { ProductFilterComponent } from '../../components/product-filter/product-filter.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
+  imports: [
+    ProductSearchComponent,
+    ProductGalleryComponent,
+    ProductFilterComponent,
+  ],
 })
 export class ProductListComponent {}
