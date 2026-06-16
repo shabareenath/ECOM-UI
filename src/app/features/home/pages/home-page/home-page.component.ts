a47ab9a820a6b953e01930727bf91931cc075e16
shabareenath <shabareenath9876@gmail.com>
2026-06-16 11:56:36 +0530

    categories updated

--- src/app/features/home/pages/home-page/home-page.component.ts
+++ src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroBannerComponent } from '../../components/hero-banner/hero-banner.component';
+import { CategoriesSectionComponent } from '../../components/categories-section/categories-section.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroBannerComponent],
+  imports: [HeroBannerComponent, CategoriesSectionComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
