8b0f2971a033db06dc158f59b52bf8fb0037a15d
shabareenath <shabareenath9876@gmail.com>
2026-06-17 14:13:56 +0530

    basic galllery updated

--- src/app/features/products/components/product-gallery/product-gallery.component.ts
+++ src/app/features/products/components/product-gallery/product-gallery.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 import { RelatedProductsComponent } from '../related-products/related-products.component';
-
+import { FormsModule } from '@angular/forms';
+import { Product } from '../../models/product.model';
 @Component({
   selector: 'app-product-gallery',
   templateUrl: './product-gallery.component.html',
   styleUrls: ['./product-gallery.component.scss'],
-  imports: [RelatedProductsComponent],
+  imports: [RelatedProductsComponent, FormsModule],
 })
-export class ProductGalleryComponent {}
+export class ProductGalleryComponent {
+  selectedSize = '';
+
+  product: Product = {
+    id: 1,
+    name: 'Oversized Black Tee',
+    price: 999,
+    image: 'assets/featured1.jpeg',
+    rating: 4.8,
+    category: 'tshirt',
+  };
+  quantity = 1;
+
+  increaseQuantity(): void {
+    this.quantity++;
+  }
+
+  decreaseQuantity(): void {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  addToCart(): void {
+    const productData = {
+      productId: 1,
+      size: this.selectedSize,
+      quantity: this.quantity,
+    };
+
+    console.log(productData);
+  }
+
+  buyNow(): void {
+    console.log('Buy Now');
+  }
+}
