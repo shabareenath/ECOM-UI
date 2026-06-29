a66cf28bc80f9e0fb4d4c685c2eaf06bbff3bbb4
shabareenath <shabareenath9876@gmail.com>
2026-06-29 22:38:46 +0530

    add new address implemented

--- src/app/features/checkout/components/addresseses/addresseses.component.ts
+++ src/app/features/checkout/components/addresseses/addresseses.component.ts
@@ -1,18 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { Address } from '../../../../core/models/address.model';
 import { AddressService } from '../../../../core/services/address.service';
 
 @Component({
   selector: 'app-address-page',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './addresseses.component.html',
   styleUrl: './addresseses.component.scss',
 })
-export class AddressesesComponent {
+export class AddressesesComponent implements OnInit {
   addresses: Address[] = [];
+
+  showForm = false;
+
+  newAddress: Address = {
+    id: 0,
+    name: '',
+    phone: '',
+    addressLine1: '',
+    addressLine2: '',
+    city: '',
+    state: '',
+    pincode: '',
+    country: '',
+    isDefault: false,
+  };
 
   private addressService = inject(AddressService);
 
-  constructor() {
+  ngOnInit(): void {
     this.loadAddresses();
   }
 
@@ -20,11 +39,45 @@ export class AddressesesComponent {
     this.addressService.getAddresses().subscribe({
       next: (addresses) => {
         this.addresses = addresses;
-        console.log(this.addresses);
       },
       error: (error) => {
-        console.error(error);
+        console.error('Failed to load addresses:', error);
       },
     });
+  }
+
+  saveAddress(): void {
+    console.log('Saving Address', this.newAddress);
+
+    // Uncomment this once addAddress() is implemented in your service.
+    /*
+    this.addressService.addAddress(this.newAddress).subscribe({
+      next: (address) => {
+        this.addresses.push(address);
+
+        this.showForm = false;
+
+        this.resetForm();
+      },
+      error: (error) => {
+        console.error('Failed to save address:', error);
+      },
+    });
+    */
+  }
+
+  resetForm(): void {
+    this.newAddress = {
+      id: 0,
+      name: '',
+      phone: '',
+      addressLine1: '',
+      addressLine2: '',
+      city: '',
+      state: '',
+      pincode: '',
+      country: '',
+      isDefault: false,
+    };
   }
 }
